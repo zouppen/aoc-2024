13eec60305c6196af1ee5dd5b541da0895532a18
Joel Lehtonen <joel.lehtonen@iki.fi>
2024-12-02 23:07:11 +0200

    Support proper JSON output

--- src/Tonttu.hs
+++ src/Tonttu.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE RecordWildCards, OverloadedStrings #-}
 module Tonttu (Tonttu(..), parseTextFile, t) where
 
+import Data.String (fromString)
+import Data.Foldable (traverse_)
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as B
 import qualified Data.Map.Strict as M
@@ -21,29 +23,33 @@ parseTextFile p f = withFile f ReadMode $ \h -> do
 tonttu :: (A.ToJSON a, A.ToJSON b, Show a, Show b) => Day a b -> Bool -> FilePath -> [String] -> IO ()
 tonttu Day{..} json file parts = do
   input <- parseTextFile parser file
-  _ <- if null parts
-    then traverse (runner input) solvers
-    else traverse (finder input) parts
-  pure ()
+  case (json, null parts) of
+    (False, True)  -> traverse_ (runner input) solvers
+    (False, False) -> traverse_ (finder input) parts
+    (True,  True)  -> jsonify $ A.object $ map (jsonRunner input) solvers
+    (True,  False) -> traverse (jsonFinder input) parts >>= jsonify . A.object
   where runner input (solverName, solver) = do
           putStr $ "Running " <> solverName <> "... "
-          case (json, solver) of
-            (False, ShowSolver f)   -> print $ f input
-            (False, StringSolver f) -> putStrLn $ f input
-            (True, ShowSolver f)    -> jsonify $ f input
-            (True, StringSolver f)  -> jsonify $ f input
-        m = M.fromList solvers
+          case solver of
+            ShowSolver f   -> print $ f input
+            StringSolver f -> putStrLn $ f input
+        jsonRunner input (solverName, solver) = case solver of
+          ShowSolver f    -> fromString solverName A..= f input
+          StringSolver f  -> fromString solverName A..= f input
+        jsonFinder input "input" = pure $ "input" A..= input
+        jsonFinder input part = case M.lookup part m of
+          Nothing -> fail $ "Part name unknown. Should be one of: " <> partList
+          Just a  -> pure $ jsonRunner input (part, a)
         finder input "input" = do
           putStr "Parser output: "
-          if json
-            then jsonify input
-            else print input
+          print input
         finder input part = case M.lookup part m of
           Nothing -> fail $ "Part name unknown. Should be one of: " <> partList
           Just a  -> runner input (part, a)
+        m = M.fromList solvers
         partList = intercalate ", " $ "input" : map fst solvers
         jsonify :: A.ToJSON a => a -> IO ()
-        jsonify a = B.hPut stdout $ "\n" <> A.encode a <> "\n"
+        jsonify a = B.hPut stdout $ A.encode a <> "\n"
 
 t :: (A.ToJSON a, A.ToJSON b, Show a, Show b) => k -> Day a b -> M.Map k Tonttu
 t i task = M.singleton i $ Tonttu $ tonttu task
