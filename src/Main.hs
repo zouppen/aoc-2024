8ae4e9d04a417ae5da25184c5396712456fb6061
Joel Lehtonen <joel.lehtonen@iki.fi>
2024-12-09 00:16:33 +0200

    Support running multiple days on one run

--- src/Main.hs
+++ src/Main.hs
@@ -1,24 +1,25 @@
 {-# LANGUAGE DeriveDataTypeable, RecordWildCards, BangPatterns #-}
 module Main where
 
-import System.Console.CmdArgs.Implicit
+import Control.Monad (forM_, unless)
 import qualified Data.Map.Strict as M
 import Data.Text (Text)
 import Data.List (intercalate)
+import System.Console.CmdArgs.Implicit
 import Text.Printf
 
 import Tontut
 
-data Args = Args { day    :: Int
+data Args = Args { days    :: [Int]
                  , input  :: Maybe FilePath
                  , part   :: [Text]
                  , json   :: Bool
                  } deriving (Show, Data, Typeable)
 
 argsDef :: Args
-argsDef = Args{ day = def &= typ "DAY" &= argPos 0
+argsDef = Args{ days = def &= typ "DAY" &= args
               , input = def &= typFile &=
-                help "File name to read for input, default is zero-padded \
+                help "File name to read for puzzle input, default is zero-padded \
                      \day under inputs/ subdirectory, e.g. inputs/02"
               , part = def &=
                 help "Solve specific part. Can be applied multiple times. \
@@ -33,10 +34,19 @@ argsDef = Args{ day = def &= typ "DAY" &= argPos 0
 main :: IO ()
 main = do
   Args{..} <- cmdArgs argsDef
-  let !file = case input of
-        Just x -> x
-        Nothing -> printf "inputs/%02d" day
-  case M.lookup day tontut of
-    Just (Tonttu t) -> t json file part
-    Nothing         -> fail $ "Not a valid day. Valid days: " <> dayList
-      where dayList = intercalate ", " $ map show $ M.keys tontut
+  -- Check argument sanity
+  case (days, input) of
+    ([_], _) -> pure ()
+    (_, Just _) -> fail "You must specify a single day to run when \
+                        \input file is specified"
+    _ -> pure ()
+  -- Start running
+  forM_ days $ \day -> do
+    unless json $ printf "-- Day %d ---\n" day
+    let !file = case input of
+          Just x -> x
+          Nothing -> printf "inputs/%02d" day
+    case M.lookup day tontut of
+      Just (Tonttu t) -> t json file part
+      Nothing         -> fail $ "Not a valid day. Valid days: " <> dayList
+        where dayList = intercalate ", " $ map show $ M.keys tontut
