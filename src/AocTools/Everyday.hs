c977d0dcaa4d343d10020760e5bb048a332ac79e
Joel Lehtonen <joel.lehtonen@iki.fi>
2024-12-20 15:07:24 +0200

    Add diamond function which basically is a functional superset of unitCoords

--- src/AocTools/Everyday.hs
+++ src/AocTools/Everyday.hs
@@ -1,9 +1,11 @@
 -- |Functions which are useful almost every day.
 module AocTools.Everyday
   ( bsearch
+  , diamond
   , minFold
   ) where
 
+import Control.Monad (guard)
 import Data.Bits
 
 bsearch :: (Ord a, Bits a, Num a) => (a -> Bool) -> a -> a -> a
@@ -21,3 +23,16 @@ minFold cmp k b (minVal, xs) = case cmp minVal b of
   LT -> (minVal, xs)   -- Ignore value
   GT -> (b, [k])       -- New minimum, throw old away
   EQ -> (minVal, k:xs) -- Collect tie values
+
+-- |Produces a possibly hollow diamond around given coordinate. Note
+-- that the hollow radius is zero based, so with radius 1 and hollow 0
+-- you'll get nearest neighbours and the node itself. With radius 1
+-- and hollow 1, you'll get nearest neighbours only. So the wall
+-- thicknes is radius-hollow+1.
+diamond :: (Enum a, Ord a, Num a) => a -> a -> (a, a) -> [(a, a)]
+diamond radius hollow (xOrigin, yOrigin) = do
+  yDelta <- [-radius..radius]
+  let xOff = radius - abs yDelta
+  xDelta <- [-xOff..xOff]
+  guard $ abs xDelta + abs yDelta >= hollow
+  pure (xOrigin + xDelta, yOrigin + yDelta)
